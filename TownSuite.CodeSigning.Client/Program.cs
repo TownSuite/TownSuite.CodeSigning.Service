6a8da3efb0badac6c5627527ba767d8c1ae092f8
Peter Gill <peter.gill@townsuite.com>
2023-03-06 15:44:28 -0330

    client, support multiple file signing

--- TownSuite.CodeSigning.Client/Program.cs
+++ TownSuite.CodeSigning.Client/Program.cs
@@ -1,12 +1,15 @@
-﻿string filepath = string.Empty;
+﻿using System.Text;
+
+string[] filepaths = null;
 string url = string.Empty;
 string token = string.Empty;
+bool quickFail = false;
 
 for (int i = 0; i < args.Length; i++)
 {
     if (string.Equals(args[i], "-file", StringComparison.InvariantCultureIgnoreCase))
     {
-        filepath = args[i + 1];
+        filepaths = args[i + 1].Split(";");
     }
     else if (string.Equals(args[i], "-url", StringComparison.InvariantCultureIgnoreCase))
     {
@@ -21,6 +24,10 @@ for (int i = 0; i < args.Length; i++)
         string tokenFile = args[i + 1];
         token = System.IO.File.ReadAllText(tokenFile);
     }
+    else if (string.Equals(args[i], "-quickfail", StringComparison.InvariantCultureIgnoreCase))
+    {
+        quickFail = true;
+    }
     else if (string.Equals(args[i], "-help", StringComparison.InvariantCultureIgnoreCase)
         || string.Equals(args[i], "--help", StringComparison.InvariantCultureIgnoreCase)
         || string.Equals(args[i], "-h", StringComparison.InvariantCultureIgnoreCase)
@@ -32,7 +39,7 @@ for (int i = 0; i < args.Length; i++)
     }
 }
 
-if (string.IsNullOrWhiteSpace(filepath))
+if (filepaths == null || filepaths.Length == 0)
 {
     Console.WriteLine("-file must be set");
     PrintHelp();
@@ -52,6 +59,7 @@ if (string.IsNullOrWhiteSpace(token))
     System.Environment.Exit(-1);
 }
 
+
 try
 {
     var client = new HttpClient
@@ -60,24 +68,39 @@ try
         Timeout = TimeSpan.FromMilliseconds(10000)
     };
 
-    var request = new HttpRequestMessage(HttpMethod.Post, url);
-    using var fs = File.OpenRead(filepath);
-    request.Content = new StreamContent(fs);
-    var response = await client.SendAsync(request);
-    fs.Close();
-    if (response.IsSuccessStatusCode)
+    bool failures = false;
+    foreach (var filepath in filepaths)
     {
-        using var resultStream = await response.Content.ReadAsStreamAsync();
-        using var memoryStream = new MemoryStream();
-        resultStream.CopyTo(memoryStream);
-        await File.WriteAllBytesAsync(filepath, memoryStream.ToArray());
-        Console.WriteLine($"Signed file: {filepath}");
+        var request = new HttpRequestMessage(HttpMethod.Post, url);
+        using var fs = File.OpenRead(filepath);
+        request.Content = new StreamContent(fs);
+        var response = await client.SendAsync(request);
+        fs.Close();
+        if (response.IsSuccessStatusCode)
+        {
+            using var resultStream = await response.Content.ReadAsStreamAsync();
+            using var memoryStream = new MemoryStream();
+            resultStream.CopyTo(memoryStream);
+            await File.WriteAllBytesAsync(filepath, memoryStream.ToArray());
+            Console.WriteLine($"Signed file: {filepath}");
+        }
+        else
+        {
+            failures = true;
+            Console.WriteLine($"Failed to sign file: {filepath}");
+            var failedOutput = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(failedOutput);
+           
+            if (quickFail)
+            {
+                Console.WriteLine("Quick fail");
+                Environment.Exit(-1);
+            }
+        }
     }
-    else
+
+    if (failures)
     {
-        Console.WriteLine("Failed to sign file");
-        var failedOutput = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(failedOutput);
         Environment.Exit(-1);
     }
 }
@@ -87,14 +110,17 @@ catch (Exception ex)
     Environment.Exit(-2);
 }
 
+
 void PrintHelp()
 {
     Console.WriteLine();
     Console.WriteLine("Options");
     Console.WriteLine("-help --help -h --h /?");
     Console.WriteLine("-file \"path to dll or exe\"");
+    Console.WriteLine("    the file path can contain multiple files by ; separating them.");
     Console.WriteLine("-url \"url to signing server\"");
     Console.WriteLine("-token \"the auth token\" or -tokenfile \"path to plain text file holding token\"");
+    Console.WriteLine("quickfail if this is set the program will exit on the first faliure.");
     Console.WriteLine("");
     Console.WriteLine("Example");
     Console.WriteLine(".\\TownSuite.CodeSigning.Client.exe -file \"C:\\some\file.dll\" -url \"https://localhost:5000/sign\" -token \"the token\"");
