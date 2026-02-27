0f44714243d5e81701c45b2ff76c0ff7ea7dfc58
Peter Gill <peter.gill@townsuite.com>
2026-02-27 16:57:30 -0330

    Improve Linux artifact packaging and workflow portability
    - Update GitHub Actions to archive both .zip and .deb artifacts for Linux builds.
    - Refactor build_linux.sh to use relative paths for better portability.
    - Add zip and SHA256SUMS generation for both client and service outputs.
    - Adjust artifact cleanup and packaging steps for consistency.

--- build_linux.sh
+++ build_linux.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-$CURRENTPATH=$(pwd)
 
 VERSION=$(cat Directory.Build.props | grep "<Version>" | sed 's/[^0-9.]*//g')
 
 dotnet restore TownSuite.CodeSigning.Service.sln
 dotnet build -c Release TownSuite.CodeSigning.Service.sln
-dotnet publish TownSuite.CodeSigning.Client -c Release -r linux-x64 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true --output $CURRENTPATH/build/linux-x64/TownSuite.CodeSigning.Client
-dotnet publish TownSuite.CodeSigning.Service -c Release -r linux-x64 --output $CURRENTPATH/build/linux-x64/TownSuite.CodeSigning.Service
+dotnet publish TownSuite.CodeSigning.Client -c Release -r linux-x64 -p:PublishReadyToRun=true --self-contained true -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true --output ./build/linux-x64/TownSuite.CodeSigning.Client
+dotnet publish TownSuite.CodeSigning.Service -c Release -r linux-x64 --output ./build/linux-x64/TownSuite.CodeSigning.Service
 
-rm -rf /build/linux
+rm -rf ./build/linux
 mkdir -p build/linux/opt/townsuite/codesigning/client
-cp -r $CURRENTPATH/build/linux-x64/TownSuite.CodeSigning.Client/* build/linux/opt/townsuite/codesigning/client/
+cp -r ./build/linux-x64/TownSuite.CodeSigning.Client/* build/linux/opt/townsuite/codesigning/client/
 mkdir -p build/linux/usr/bin
 cp -r townsuite-code-signing-client build/linux/usr/bin/townsuite-code-signing-client
 chmod +x build/linux/usr/bin/townsuite-code-signing-client
@@ -33,10 +32,12 @@ fpm -s dir -t deb \
   ./
 
 # zip the linux-x64 folder
-cd $CURRENTPATH/build
+cd ..
 zip -r TownSuite.CodeSigning.Client-$VERSION-linux-x64.zip linux-x64/TownSuite.CodeSigning.Client
+zip -r TownSuite.CodeSigning.Service-$VERSION-linux-x64.zip linux-x64/TownSuite.CodeSigning.Service
 
 # create *.SHA256SUMS per file
 sha256sum TownSuite.CodeSigning.Client-$VERSION-linux-x64.zip > TownSuite.CodeSigning.Client-$VERSION-linux-x64.zip.SHA256SUMS
+sha256sum TownSuite.CodeSigning.Service-$VERSION-linux-x64.zip > TownSuite.CodeSigning.Service-$VERSION-linux-x64.zip.SHA256SUMS
 
-cd $CURRENTPATH
+cd ..
